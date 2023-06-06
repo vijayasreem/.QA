00178ad7b32c89a09427d434aee04c803e419602
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-06 16:13:13 +0530

    initial commit

---
+++ force-app/main/default/aura/SalesReportGenerator.js
@@ -0,0 +1,11 @@
+//TestUserStory.js
+({
+    doInit : function(component, event, helper) {
+        //Initialize component data
+        helper.initData(component);
+    },
+    handleOnClick : function(component, event, helper) {
+        //Handle OnClick Action
+        helper.handleOnClick(component, event);
+    }
+})
