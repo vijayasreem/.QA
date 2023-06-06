06d4c24385b4782d3e4e726f89931fa2c5b0ccad
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-06 11:34:47 +0530

    initial commit

--- app/app.py
+++ app/app.py
@@ -1,31 +1,40 @@
-from flask import Flask, jsonify, request
+@app.route('/payment', methods=['GET', 'POST'])
+def payment():
+  
+  # Handle GET request
+  if request.method == 'GET':
+    return render_template('payment.html')
+  
+  # Handle POST request
+  if request.method == 'POST':
+    # Get user's payment details
+    payment_method = request.form.get('payment_method')
+    card_number = request.form.get('card_number')
+    expiry_date = request.form.get('expiry_date')
+    cvv = request.form.get('cvv')
+    
+    # Redirect user to secure payment page
+    if payment_method == 'paypal':
+      return redirect(url_for('paypal'))
+    elif payment_method == 'stripe':
+      return redirect(url_for('stripe'))
+    else: # Credit/debit card payment
+      # Validate user's payment details
+      if card_number and expiry_date and cvv:
+        # Process payment
+        result = process_payment(card_number, expiry_date, cvv)
+        if result == 'success':
+          # Payment was successful
+          return redirect(url_for('payment_success'))
+        else:
+          # Payment failed
+          return redirect(url_for('payment_failed'))
+      else:
+        # Invalid payment details
+        flash('Please enter valid payment details')
+        return redirect(url_for('payment'))
 
-app = Flask(__name__)
-
-@app.route('/validate-policy', methods=['POST'])
-def validate_policy():
-    # Get the data from the request
-    data = request.get_json()
-    # Check if the data is valid
-    if not data:
-        return jsonify({'message': 'No data provided'}), 400
-    # Check if all the necessary fields are present
-    if 'sum_assured' not in data or 'age' not in data or 'annual_income' not in data or 'otp' not in data:
-        return jsonify({'message': 'Not all required data provided'}), 400
-    # Check if the sum assured is within the valid range
-    if data['sum_assured'] < 50000 or data['sum_assured'] > 200000:
-        return jsonify({'message': 'Sum assured not in valid range'}), 400
-    # Check if the age is within the valid range
-    if data['age'] < 18 or data['age'] > 60:
-        return jsonify({'message': 'Age not in valid range'}), 400
-    # Check if the annual income is at least 40K
-    if data['annual_income'] < 40000:
-        return jsonify({'message': 'Annual income too low'}), 400
-    # Check if the OTP is valid
-    if not data['otp']:
-        return jsonify({'message': 'OTP not provided'}), 400
-    # All validations have passed
-    return jsonify({'message': 'Validation successful'}), 200
-
-if __name__ == '__main__':
-    app.run()
+# Function to process payment
+def process_payment(card_number, expiry_date, cvv):
+  # TODO: Implement payment processing logic
+  return 'success' # or 'failure'
